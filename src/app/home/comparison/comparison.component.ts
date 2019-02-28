be9657fa709bae464051453025d52c539fd7c7a0
Godstime Osarobo <nosarobo@gmail.com>
2019-02-28 01:25:36 +0000

    Pitch can now be gotten from vocal wav files

--- src/app/home/comparison/comparison.component.ts
+++ src/app/home/comparison/comparison.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import Chart from 'chart.js';
+import Chart, { InteractionMode, ChartConfiguration } from 'chart.js';
 import * as Pitchfinder from 'pitchfinder';
 import WavDecoder from 'wav-decoder';
 @Component({
@@ -27,19 +27,16 @@ export class ComparisonComponent implements OnInit {
   }
   constructor() {
     const self = this;
+    // when the file is read
     this.fileReader.onload = (e: any) => {
-      console.log(e);
+      // stores the file as an ArrayBuffer
       self.arrayBuffer = e.currentTarget.result;
       console.log('ArrayBuffer: ', self.arrayBuffer);
       console.log('Byte Length: ' + self.arrayBuffer.byteLength);
-      // get audio data form file using wavDecoder
+      // get audio data form file using WavDecoder
       const decodedWav = WavDecoder.decode.sync(self.arrayBuffer);
       console.log('Sample Rate: ', decodedWav.sampleRate);
-      // console.log('Channel Data 0: ', decodedWav.channelData[0]); // Float32Array
-      // console.log('Channel Data 1: ', audioData.channelData[1]); // Float32Array
-      // const pitch = self.detectPitch(decodedWav.channelData[0]); // null if pitch cannot be identified
-      // or use multiple detectors for better accuracy at the cost of speed.
-      const detectors = [self.detectPitch, Pitchfinder.AMDF()];
+      const detectors = [Pitchfinder.YIN()];
       let moreAccurateFrequencies = Pitchfinder.frequencies(
         detectors,
         decodedWav.channelData[0],
@@ -48,116 +45,41 @@ export class ComparisonComponent implements OnInit {
           quantization: 4, // samples per beat, defaults to 4 (i.e. 16th notes)
         }
       );
-      moreAccurateFrequencies =
-        // [
-        //   17640.395528936995,
-        //   18726.972774623853,
-        //   210,
-        //   212.12277483468188,
-        //   234.5937743620731,
-        //   18454.110180588144,
-        //   281.1160910016887,
-        //   277.4343256108928,
-        //   283.30533337205463,
-        //   280.47114053065536,
-        //   280.8846737684757,
-        //   282.01989876947385,
-        //   279.564367380423,
-        //   281.9702747122019,
-        //   0,
-        //   0,
-        //   281.8279065120751,
-        //   278.20077871517884,
-        //   278.38408350070523,
-        //   312.12665025285503,
-        //   330.1659354063537,
-        //   0,
-        //   284.9978920125136,
-        //   0,
-        //   318.82445758558754,
-        //   356.10281590054433,
-        //   348.57414745722696,
-        //   353.7549022929497,
-        //   352.6164957272168,
-        //   0,
-        //   0,
-        //   0,
-        //   319.38047239137677,
-        //   353.29785527682094,
-        //   352.399556844769,
-        //   351.578487078433,
-        //   353.70538363345617,
-        //   352.657657844728,
-        //   350.61807505248055,
-        //   356.5070266463866,
-        //   356.57823447486146,
-        //   355.1522291167095,
-        //   0,
-        //   18557.02854455429,
-        //   18402.560463070815,
-        //   17923.635388751045,
-        //   17827.67823459781,
-        //   0,
-        //   18164.967011454395,
-        //   208.95158762649635,
-        //   213.36663539012926,
-        //   210.48385833703082,
-        //   206.03373003928812,
-        //   0,
-        //   239.67847467425364,
-        //   279.5193544893314,
-        //   280.0979129887348,
-        //   279.11392405063293,
-        //   18630.15450411411,
-        //   286.460504058385,
-        //   232.18910368658288,
-        //   18670.748917040208,
-        //   209.69877284162712,
-        //   18079.728508373006,
-        //   0,
-        //   17916.056621886884,
-        //   17994.077578443532,
-        //   18677.42310924685,
-        //   0,
-        //   0,
-        //   0,
-        //   231.5588714445374,
-        //   233.42755078747862,
-        //   235.64257982659967,
-        //   0,
-        //   276.18362805992916,
-        //   281.1754277180157,
-        //   280.9761764707367,
-        //   279.1425053802541,
-        //   317.123905222104,
-        //   0,
-        //   0,
-        //   281.5203725323482,
-        //   284.1150132672389,
-        //   282.1898285266621,
-        //   279.22708227355866,
-        //   283.23267461330204,
-        //   280.96549262944075,
-        //   280.7487641596633,
-        //   18136.555084571937,
-        // ]
-        moreAccurateFrequencies.map((element, index) => ({
+      /* vocal test data pre-calculated */
+      /*[17640.395528936995,18726.972774623853,210,212.12277483468188,234.5937743620731,18454.110180588144,281.1160910016887,277.4343256108928,283.30533337205463,280.47114053065536,280.8846737684757,282.01989876947385,279.564367380423,281.9702747122019,0,0,281.8279065120751,278.20077871517884,278.38408350070523,312.12665025285503,330.1659354063537,0,284.9978920125136,0,318.82445758558754,356.10281590054433,348.57414745722696,353.7549022929497,352.6164957272168,0,0,0,319.38047239137677,353.29785527682094,352.399556844769,351.578487078433,353.70538363345617,352.657657844728,350.61807505248055,356.5070266463866,356.57823447486146,355.1522291167095,0,18557.02854455429,18402.560463070815,17923.635388751045,17827.67823459781,0,18164.967011454395,208.95158762649635,213.36663539012926,210.48385833703082,206.03373003928812,0,239.67847467425364,279.5193544893314,280.0979129887348,279.11392405063293,18630.15450411411,286.460504058385,232.18910368658288,18670.748917040208,209.69877284162712,18079.728508373006,0,17916.056621886884,17994.077578443532,18677.42310924685,0,0,0,231.5588714445374,233.42755078747862,235.64257982659967,0,276.18362805992916,281.1754277180157,280.9761764707367,279.1425053802541,317.123905222104,0,0,281.5203725323482,284.1150132672389,282.1898285266621,279.22708227355866,283.23267461330204,280.96549262944075,280.7487641596633,18136.555084571937]*/
+      moreAccurateFrequencies = moreAccurateFrequencies.map(
+        (element, index) => ({
           x: index,
           y: element,
-        }));
-      // console.log('Frequencies:' +  this.frequencies.toString());
-      console.log(
-        'Accurate Frequencies:' +
-          moreAccurateFrequencies.map(e => JSON.stringify(e)).toString()
+        })
       );
-      const options = {
-        container: '.container',
+      // console.log(
+      //   'Accurate Frequencies:' +
+      //     moreAccurateFrequencies.map(e => JSON.stringify(e)).toString()
+      // );
+      const ctx: any = document.getElementById('VocalChart');
+      const LineConfig: ChartConfiguration = {
+        type: 'line',
         data: {
-          values: moreAccurateFrequencies,
+          labels: moreAccurateFrequencies.map(xy => xy.x),
+          datasets: [
+            {
+              data: moreAccurateFrequencies.map(xy => xy.y),
+              label: 'Vocal Pitches',
+              borderColor: '#ffab40',
+              fill: false,
+            },
+          ],
+        },
+        options: {
+          title: {
+            display: true,
+            text: 'Chart of Vocal Pitches',
+          },
+          legend: { display: false },
         },
       };
-      const ctx = document.getElementById('myChart');
-      const config = {
+      const ScatterConfig: ChartConfiguration = {
         type: 'scatter',
         data: {
           datasets: [
@@ -176,23 +98,24 @@ export class ComparisonComponent implements OnInit {
             display: true,
             text: 'Chart.js Line Chart',
           },
+          legend: { display: false },
           tooltips: {
-            mode: 'index',
+            mode: 'index' as InteractionMode,
             intersect: false,
           },
           hover: {
-            mode: 'nearest',
+            mode: 'nearest' as InteractionMode,
             intersect: true,
           },
           scales: {
             xAxes: [
               {
                 type: 'linear',
-                position: 'bottom',
-                display: true,
+                display: false,
                 scaleLabel: {
                   display: true,
-                  labelString: 'Time',
+                  labelString: 'Index',
+                  fontColor: 'white',
                 },
               },
             ],
@@ -202,13 +125,14 @@ export class ComparisonComponent implements OnInit {
                 scaleLabel: {
                   display: true,
                   labelString: 'Frequency',
+                  fontColor: 'white',
                 },
               },
             ],
           },
         },
       };
-      const frequencyChart = new Chart(ctx, config);
+      const frequencyChart = new Chart(ctx, ScatterConfig);
     };
   }
 
